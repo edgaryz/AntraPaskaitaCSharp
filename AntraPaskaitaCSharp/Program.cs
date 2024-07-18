d531e148a5ff2001412c3446cbcf7452b2ae42be
Edgar Sokol <edgar.sokol@gmail.com>
2024-07-18 14:15:30 +0300

    first three tasks

--- AntraPaskaitaCSharp/Program.cs
+++ AntraPaskaitaCSharp/Program.cs
@@ -1,13 +1,78 @@
-﻿using System;
-
-namespace LearningObjects
+﻿namespace LearningObjects
 {
-    
+
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            /* task 1 */
+            /*CheckNumber(5);
+            CheckNumber(-2);
+            CheckNumber(0);*/
+
+            /* task 2 */
+            /*Console.WriteLine(HighestValue(2, 5, 2));
+            Console.WriteLine(HighestValue(2, 5, 3));
+            Console.WriteLine(HighestValue(2, 2, 2));*/
+
+            /* task 3 */
+            /*CheckSymbol("a");
+            CheckSymbol("b");*/
         }
+
+        /* task 1 */
+        /*public static void CheckNumber(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine("Teigiamas");
+            }
+            else if (number < 0) {
+                Console.WriteLine("Neigiamas");
+            } 
+            else
+            {
+                Console.WriteLine("Nulis");
+            }
+
+
+        }*/
+
+        /* task 2 */
+        /*public static int HighestValue(int num1, int num2, int num3)
+        {
+            if (num1 > num2 && num1 > num3)
+            {
+                return num1;
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                return num2;
+            }
+            else if (num3 > num1 && num3 > num2)
+            {
+                return num3;
+            }
+            else
+            {
+                Console.WriteLine("Error, maybe all number are equal");
+                return 0;
+                
+            }
+
+        }*/
+
+        /* task 3 */
+        /*public static void CheckSymbol(string symbol)
+        {
+            if (symbol == "a" || symbol == "e" || symbol == "i" || symbol == "u" || symbol == "o")
+            {
+                Console.WriteLine("Balsė");
+            }
+            else
+            {
+                Console.WriteLine("Priebalsė");
+            }
+        }*/
     }
 }
